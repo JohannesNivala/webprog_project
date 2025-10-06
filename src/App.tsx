2dd56af5470668fbda0283d6e4ff184f209b5eb2
JohannesNivala <norenjojo02@gmail.com>
2025-10-06 13:27:14 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline mb-2">
+        Hello tailwind world!
+      </h1>
+      <Button variant="outline">Hello shadcn world</Button>
+    </>
+  );
+}
+
+export default App;
